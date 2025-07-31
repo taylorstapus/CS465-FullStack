804422af4a24b326195c2ccb7a32ab0a887435f3
Taylor Stapus <tstapus@gmail.com>
2025-07-31 15:45:50 -0700

    Module 5 completed baseline

---
+++ app_api/controllers/travel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); //Register model
+const Model = mongoose.model('trips');
+
+// Get: /trips - list of all trips
+const tripsList = async(req, res) => {
+  const q = await Model
+    .find({'code' : req.params.tripCode}) //return single record 
+    .exec();
+
+    console.log(q);
+
+  if(!q)
+  {//Database return no data
+    return res
+      .status(404)
+      .json(err);
+  }
+  else{ // Return resulting trip list 
+    return res
+      .status(200)
+      .json(q);
+  } 
+};
+
+module.exports = {
+  tripsList, 
+  tripsFindByCode
+};
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,36 @@
-var fs = require('fs');
+const tripsEndpoint = "http://localhost:3000/api/trips"; 
+const options = { 
+  method: "GET", 
+  headers: { 
+    Accept: "application/json", 
+  }, 
+}; 
 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+//var fs = require('fs');
+
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 
 /* GET travel view */
-const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways', trips });
+const travel = async function (req, res, next) {
+  await fetch(tripsEndpoint, options)
+  .then((res) => res.json())
+  .then((json) => {
+    let message = null;
+    if(!(json instanceof Array)) {
+      message = "API lookup error";
+      json = [];
+    }
+    else {
+      if (!json.length) {
+        message = "No trips exists in our database!";
+      }
+    }
+    res.render('travel', { title: 'Travlr Getaways', trips: json, message });
+  })
+  .catch((err) => res.status(500).send(err.message));
 };
+
 module.exports = {
   travel
 };
